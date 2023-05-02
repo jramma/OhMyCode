cd38aa662c9243bc568d47bfe03507f3823d3ce5
jramma <jrmasip97@gmail.com>
2023-05-02 11:36:15 +0200

    first commit

---
+++ src/main/java/oh/mycode/ohmycode/OhmycodeApplication.java
@@ -0,0 +1,13 @@
+package oh.mycode.ohmycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhmycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhmycodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oh/mycode/ohmycode/OhmycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package oh.mycode.ohmycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhmycodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
